c2d02f0909b8736ece83ecc3ab8255348a0d279e
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-01-21 11:45:15 -0800

    Add project files.

---
+++ SchoolTracker/Payroll.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolTracker
+{
+    interface IPayee
+    {
+        void Pay();
+    }
+    public class Payroll
+    {
+        List<IPayee> payees = new List<IPayee>();
+        public Payroll()
+        {
+            payees.Add(new Teacher());
+            payees.Add(new Teacher());
+            payees.Add(new Principal());
+        }
+        
+
+        public void PayAll()
+        {
+            foreach (var payee in payees)
+            {
+                payee.Pay();
+            }
+        }
+    }
+}
